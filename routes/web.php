df9767e3bba7745951dd50c8bde343b3df849675
alexchtst-01 <acinatra@gmail.com>
2025-01-26 07:19:56 +0700

    add shadcn and make dashboard layout for eo

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/eodashboard', function () {
+    return Inertia::render('Eodashboard/Eodashboard');
+})->middleware(['auth', 'verified'])->name('dashboard')
+->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
