6f59c77edb0318371da0599aea3d2f3a170744e5
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-02-25 18:06:47 +0700

    fix actions vite format

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -78,7 +78,7 @@ export default function Login({
                             name="remember"
                             checked={data.remember}
                             onChange={(e) =>
-                                setData('remember', e.target.checked)
+                                setData('remember', e.target.checked as boolean)
                             }
                         />
                         <span className="ms-2 text-sm text-gray-600">
