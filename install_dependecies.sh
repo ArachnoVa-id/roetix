912fa1364adef2269927cdcde5ffa28e93e47adb
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-02-25 17:18:27 +0700

    fix missing dependancy

--- install_dependecies.sh
+++ install_dependecies.sh
@@ -42,7 +42,7 @@ if ! command -v php &> /dev/null || [[ $(php -v | grep -oP '(?<=PHP )\d+\.\d+') 
     echo "🛠 Installing PHP 8.2 and extensions..."
     sudo add-apt-repository ppa:ondrej/php -y
     sudo apt update -y
-    sudo apt install -y php8.2 php8.2-fpm php8.2-cli php8.2-mbstring php8.2-xml php8.2-bcmath php8.2-curl php8.2-zip php8.2-intl php8.2-gd
+    sudo apt install -y php8.2 php8.2-fpm php8.2-cli php8.2-mbstring php8.2-xml php8.2-bcmath php8.2-curl php8.2-zip php8.2-intl php8.2-gd php8.2-mysql
     sudo systemctl enable php8.2-fpm
     sudo systemctl start php8.2-fpm
 else
