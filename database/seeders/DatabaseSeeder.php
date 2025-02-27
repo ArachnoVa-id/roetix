a6f22eb90fe8624c8abced1366f40419f2cc6446
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-02-27 19:56:43 +0700

    fix tryfix actions laravel cloning

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Coupon;
-use App\Models\Event;
-use App\Models\User;
-use App\Models\EventVariables;
-use App\Models\Order;
-use App\Models\Seat;
-use App\Models\Ticket;
-use App\Models\TicketCategory;
-use App\Models\SeatSeeder;
-use App\Models\Team;
-use App\Models\UserContact;
-use App\Models\Venue;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
