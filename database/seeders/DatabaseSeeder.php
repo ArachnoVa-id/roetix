6928524f154139704ef0db8f3513ccceb29b12fb
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-02-27 22:10:58 +0700

    fix uncomment truncates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Coupon;
+use App\Models\Event;
+use App\Models\EventVariables;
+use App\Models\Order;
+use App\Models\Team;
+use App\Models\Ticket;
+use App\Models\TicketCategory;
+use App\Models\User;
+use App\Models\UserContact;
+use App\Models\Venue;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,21 +22,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-        // Team::truncate();
-        // Order::truncate();
-        // Coupon::truncate();
-        // TicketCategory::truncate();
-        // Ticket::truncate();
-        // Seat::truncate();
-        // Venue::truncate();
-        // UserContact::truncate();
-        // EventVariables::truncate();
-        // Event::truncate();
-        // User::truncate();
+        Team::truncate();
+        Order::truncate();
+        Coupon::truncate();
+        TicketCategory::truncate();
+        Ticket::truncate();
+        Venue::truncate();
+        UserContact::truncate();
+        EventVariables::truncate();
+        Event::truncate();
+        User::truncate();
 
-        // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
 
         $this->call([
