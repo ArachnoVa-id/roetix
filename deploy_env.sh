d5e55f3393c4f5ae6224c1ce4fa1ec7d29c5f9e0
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-03-09 21:42:37 +0700

    fix env to vps from scp only

--- deploy_env.sh
+++ deploy_env.sh
@@ -9,7 +9,7 @@ STAGING_DIR="~/novatix/app/develop"
 PRODUCTION_DIR="~/novatix/app/production"
 
 # Copy .env.staging to the staging directory
-scp -i ~/.ssh/id_rsa .env.staging "$VPS_USER@$VPS_IP:$STAGING_DIR/.env.staging"
+scp -i ~/.ssh/id_rsa .env.staging "$VPS_USER@$VPS_IP:$STAGING_DIR/.env"
 
 # Copy .env.production to the production directory
-scp -i ~/.ssh/id_rsa .env.production "$VPS_USER@$VPS_IP:$PRODUCTION_DIR/.env.production"
+scp -i ~/.ssh/id_rsa .env.production "$VPS_USER@$VPS_IP:$PRODUCTION_DIR/.env"
