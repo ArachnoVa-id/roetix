07bf1d09e4b6ac4e2e7f86aa3537e9f57ef6344a
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-03-11 11:05:52 +0700

    feat simple workflow

--- app/Filament/Admin/Resources/VenueResource/Pages/CreateVenue.php
+++ app/Filament/Admin/Resources/VenueResource/Pages/CreateVenue.php
@@ -2,11 +2,30 @@
 
 namespace App\Filament\Admin\Resources\VenueResource\Pages;
 
-use App\Filament\Admin\Resources\VenueResource;
 use Filament\Actions;
+use App\Models\UserContact;
+use Illuminate\Support\Str;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Admin\Resources\VenueResource;
 
 class CreateVenue extends CreateRecord
 {
     protected static string $resource = VenueResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Create new UserContact
+        $contact = UserContact::create([
+            'contact_id' => Str::uuid(),
+            'phone_number' => $data['phone_number'],
+            'email' => $data['email'],
+            'whatsapp_number' => $data['whatsapp_number'] ?? null,
+            'instagram' => $data['instagram'] ?? null,
+        ]);
+
+        // Assign the new contact to the venue data
+        $data['contact_info'] = $contact->contact_id;
+
+        return $data;
+    }
 }
