19ca918249f6536bdded6fe23f265ab6cc48565a
alexchtst-01 <acinatra@gmail.com>
2025-03-17 17:28:27 +0700

    feat: unable the csrf token in payment

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -18,6 +18,7 @@ class VerifyCsrfToken extends Middleware
         // to properly handle CSRF tokens
         // 'payment/webhook', // Example: exclude payment webhooks
         'payment/charge',
+        'payment/callback',
     ];
     
     /**
