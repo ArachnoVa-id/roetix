84cc09ed36df316b5a64f33201f97e59bf2c5871
alexchtst-01 <acinatra@gmail.com>
2025-03-24 23:03:19 +0700

    feat minor fix

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -36,11 +36,6 @@ class Venue extends Model
             if (empty($model->venue_id)) {
                 $model->venue_id = (string) Str::uuid();
             }
-            
-            // protect from model
-            if ($team && $team->venues()->count() >= $team->vendor_quota) {
-                throw new \Exception("This team has reached its venue limit.");
-            }
         });
     }
 
