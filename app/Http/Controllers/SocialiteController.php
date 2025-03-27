3537bb0a66e60038cdf682e7528404dce42e1828
alexchtst-01 <acinatra@gmail.com>
2025-03-28 02:05:20 +0700

    feat: fixing the google auth

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -13,23 +13,28 @@ use Illuminate\Support\Str;
 
 class SocialiteController extends Controller
 {
-    public function googleLogin()
+    public function googleLogin($client)
     {
         // dd(request()->fullUrl());
+        session(['client' => $client]);
         return Socialite::driver('google')->redirect();
     }
 
     public function googleAuthentication()
     {
+        $client = session('client');
+
         try {
             $google_user = Socialite::driver('google')->user();
 
-            $user = User::where('google_id', $google_user->id)->first();
+            $user = User::where('google_id', $google_user->id)
+                        ->orWhere('email', $google_user->email)
+                        ->first();
 
             if ($user) {
                 Auth::login($user);
 
-                return redirect()->route('client.home');
+                return redirect()->route('client.home', ['client' => $client]);
             } else {
                 $userdata = User::create([
                     'id' => (string) Str::uuid(),
@@ -42,12 +47,11 @@ class SocialiteController extends Controller
 
                 if ($userdata) {
                     Auth::login($userdata);
-                    return redirect()->route('client.home');
+                    return redirect()->route('client.home', $client);
                 }
             }
         } catch (Exception $e) {
-            // dd($e);
+            return redirect()->route('auth.google')->with('error', 'Google login failed: ' . $e->getMessage());
         }
-        // dd($google_user);
     }
 }
--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -8,6 +8,7 @@ import { Event, EventProps } from '@/types/front-end';
 import { Head, Link, useForm } from '@inertiajs/react';
 import axios from 'axios';
 import { FormEventHandler, useEffect } from 'react';
+import { MailIcon } from 'lucide-react';
 
 export default function Login({
     status,
@@ -62,82 +63,94 @@ export default function Login({
             )}
 
             <div className='mb-6 text-center font-bold'>{event.name}</div>
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+            {event.name === 'Admin NovaTix' ? (
+                <form onSubmit={submit}>
+                    <div>
+                        <InputLabel htmlFor="email" value="Email" />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setData('email', e.target.value)
-                        }
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                            setData('password', e.target.value)
-                        }
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(
-                                e: React.ChangeEvent<HTMLInputElement>,
-                            ) =>
-                                setData('remember', e.target.checked as boolean)
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="mt-1 block w-full"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setData('email', e.target.value)
                             }
                         />
-                        <span className="ms-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
 
-                <div className="mt-4 flex items-center justify-end">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
+                        <InputError message={errors.email} className="mt-2" />
+                    </div>
 
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
+                    <div className="mt-4">
+                        <InputLabel htmlFor="password" value="Password" />
+
+                        <TextInput
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="mt-1 block w-full"
+                            autoComplete="current-password"
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setData('password', e.target.value)
+                            }
+                        />
+
+                        <InputError message={errors.password} className="mt-2" />
+                    </div>
+
+                    <div className="mt-4 block">
+                        <label className="flex items-center">
+                            <Checkbox
+                                name="remember"
+                                checked={data.remember}
+                                onChange={(
+                                    e: React.ChangeEvent<HTMLInputElement>,
+                                ) =>
+                                    setData('remember', e.target.checked as boolean)
+                                }
+                            />
+                            <span className="ms-2 text-sm text-gray-600">
+                                Remember me
+                            </span>
+                        </label>
+                    </div>
+
+                    <div className="mt-4 flex items-center justify-end">
+                        {canResetPassword && (
+                            <Link
+                                href={route('password.request')}
+                                className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                Forgot your password?
+                            </Link>
+                        )}
+
+                        <PrimaryButton className="ms-4" disabled={processing}>
+                            Log in
+                        </PrimaryButton>
+                    </div>
+                    {/* <a
+                        href={route('auth.google', {}, false)}
+                        className="mt-2 w-full text-center"
+                    >
+                        Login With Google
+                    </a> */}
+                </form>
+            ) : (
+                <div className='flex'>
+                    <a
+                        href={route('auth.google', {}, false)}
+                        className="mt-2 w-full text-center bg-red-700 text-white font-bold p-[1vw] rounded-md flex space-x-4 justify-center"
+                    >
+                        <MailIcon />
+                        <span>Login With Google</span>
+                    </a>
                 </div>
-                <a
-                    href={route('auth.google', {}, false)}
-                    className="mt-2 w-full text-center"
-                >
-                    Login With Google
-                </a>
-            </form>
+            )}
         </GuestLayout>
     );
 }
