67632439383003b94dbedba4f18df4aaa8258168
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-05-10 16:04:24 +0700

    fix some configs and db setups

--- database/migrations/v1_0_0_beta/0017_update_event_variables_table.php
+++ database/migrations/v1_0_0_beta/0017_update_event_variables_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,6 +16,12 @@ return new class extends Migration
             $table->integer('active_users_threshold')->default(100)->after('ticket_limit');
             $table->integer('active_users_duration')->default(10)->after('active_users_threshold');
         });
+
+        // Populate existing rows with default values for the new columns
+        DB::table('event_variables')->update([
+            'active_users_threshold' => 100,
+            'active_users_duration' => 10,
+        ]);
     }
 
     /**
