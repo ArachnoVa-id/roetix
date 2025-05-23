0d693e6694976ab5681911f9b0171404d442e136
alexchtst-01 <acinatra@gmail.com>
2025-05-23 22:01:21 +0700

    feat: add new font and images for enhanced UI design

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                unageo: ['unageo', ...defaultTheme.fontFamily.sans],
             },
             borderRadius: {
                 lg: 'var(--radius)',
