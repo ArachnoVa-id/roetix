97f87a85182f26e77236c873b5c5e208921fb1de
fabian4819 <habibfabianfahlesi2004@Mail.ugm.ac.id>
2025-05-27 18:33:31 +0700

    fix: user isreceptionist

--- app/Models/User.php
+++ app/Models/User.php
@@ -120,9 +120,9 @@ class User extends Authenticatable implements FilamentUser, HasName, HasTenants,
     public function isReceptionist(): bool
     {
         return $this->role === UserRole::RECEPTIONIST->value ||
-            $this->role === 'receptionist' ||
-            $this->role == UserRole::ADMIN->value ||
-            $this->role === 'admin'; // Admin can also scan tickets
+            $this->role === 'receptionist';
+            // $this->role == UserRole::ADMIN->value ||
+            // $this->role === 'admin'; // Admin can also scan tickets
     }
 
     public function isEO(): bool
